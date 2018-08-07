b224bd3ad041fca407f69203e65547a6b86e2264
DoubleThink1984 <d.a.cook1@outlook.com>
2018-08-07 16:52:56 -0500

    Add project files.

---
+++ MSMQInterceptor/MSMQHelper.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Messaging;
+using System.Messaging.Design;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace MSMQInterceptor
+{
+    public static class MSMQHelper
+    {
+        static readonly string path = @"C:\Users\devin.PSC\Documents\TestMessageXml\";
+
+        public static void ProcessMessageQueue(string queuePath)
+        {
+            using (MessageQueue msgQ = new MessageQueue(queuePath))
+            {
+                if (msgQ != null)
+                {
+                    Message[] messages = msgQ.GetAllMessages();
+
+                    foreach (var message in messages)
+                    {
+                        ProcessMessage(message);
+                    }
+                }                
+            }                        
+        }
+
+        public static void ProcessMessage(Message message)
+        {
+            var fileName = path + message.LookupId.ToString() + ".xml";
+            var doc = GetXmlDocFromMsg(message);
+            doc.Save(fileName);
+        }
+                
+        private static XmlDocument GetXmlDocFromMsg(Message message)
+        {
+            XmlDocument doc = new XmlDocument();
+            message.Formatter = new ActiveXMessageFormatter();
+            using (var sw = new StreamReader(message.BodyStream))
+            {
+                try
+                {
+                    var msgBody = sw.ReadToEnd();
+                    if (!string.IsNullOrWhiteSpace(msgBody))
+                    {
+                        doc = GetXmlDocFromString(msgBody);
+                    }
+                }
+                catch (Exception e)
+                {
+                    System.Diagnostics.Debug.Write(e.Message);
+                }
+                return doc;
+            }
+        }
+
+        private static XmlDocument GetXmlDocFromString(string xmlString)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(xmlString);
+            return doc;
+        }
+    }
+}
---
+++ MSMQInterceptor/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace MSMQInterceptor
+{
+    class Program
+    {
+        static readonly string quePath = @"dell47\private$\testqueue";
+
+        static void Main(string[] args)
+        {
+            SendMessages();
+            MSMQHelper.ProcessMessageQueue(quePath);
+        }
+
+        static void SendMessages()
+        {            
+            MessageQueue messageQueue = null;
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(Directory.GetCurrentDirectory() + @"\XMLFiles\TestFile.xml");
+            if (MessageQueue.Exists(quePath))
+            {
+                messageQueue = new MessageQueue(quePath);
+                messageQueue.Label = "Testing Queue";
+            }
+            else
+            {
+                // Create the Queue
+                MessageQueue.Create(quePath);
+                messageQueue = new MessageQueue(quePath);
+                messageQueue.Label = "Newly Created Queue";
+            }
+            System.Messaging.Message msg = new System.Messaging.Message
+            {
+                Body = xmlDoc,
+                Formatter = new XmlMessageFormatter(new String[] { "System.String,mscorlib" })
+            };
+
+            messageQueue.Send(msg);
+        }
+    }
+}
